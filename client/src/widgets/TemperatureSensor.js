a0775beeb48210b03eb6187f374d226ac6228321
felixwaldbach <felixwaldbach@shootingstar-3.local>
2018-12-04 13:05:43 +0100

    Added chart function to display temperature as charts

--- client/src/widgets/TemperatureSensor.js
+++ client/src/widgets/TemperatureSensor.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import '../font/css/custom.css';
 import socketIOClient from "socket.io-client";
 
+var LineChart = require("react-chartjs").Line;
+
 class TemperatureSensor extends Component {
 
     constructor(props) {
@@ -10,7 +12,22 @@ class TemperatureSensor extends Component {
         this.state = {
             style: props.style,
             endpoint: 'http://localhost:5000',
-            temperature: 0
+            temperature: 0,
+            data: {
+                labels: ["0", "10", "20", "30", "40", "50", "60"],
+                datasets: [
+                    {
+                        label: "Daily Temperature Data Set",
+                        fillColor: "rgba(220,220,220,0.2)",
+                        strokeColor: "rgba(220,220,220,1)",
+                        pointColor: "rgba(220,220,220,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(220,220,220,1)",
+                        data: [0]
+                    }
+                ]
+            }
         }
     }
 
@@ -21,14 +38,23 @@ class TemperatureSensor extends Component {
             app.setState({
                 temperature: data
             });
+            app.addTemperatureData(data);
+        });
+    }
+
+    addTemperatureData(temperature) {
+        let data = this.state.data;
+        data.datasets[0].data.push(temperature);
+        this.setState({
+            data: data
         });
     }
 
     render() {
         return (
             <div className="temperature-sensor-container">
-                <h1>Temperature:</h1>
-                <p>{this.state.temperature} ° C</p>
+                <h1>Temperature: {this.state.temperature} ° C</h1>
+                <LineChart data={this.state.data} width="300" height="125"/>
             </div>
         );
     }
